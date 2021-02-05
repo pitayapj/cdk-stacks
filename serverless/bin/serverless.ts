a58b800e1c906d796792f81bc046846a3eb6434f
pepeg <hoanglong2205@gmail.com>
2021-02-06 00:32:16 +0900

    pls

--- serverless/bin/serverless.ts
+++ serverless/bin/serverless.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { CiCdStack } from '../lib/ci-cd-stack';
+import { ServerlessStack } from '../lib/serverless-stack';
 
 const app = new cdk.App();
-new CiCdStack(app, 'CiCdStack');
+new ServerlessStack(app, 'ServerlessStack');
--- serverless/test/serverless.test.ts
+++ serverless/test/serverless.test.ts
@@ -1,11 +1,11 @@
 import { expect as expectCDK, matchTemplate, MatchStyle } from '@aws-cdk/assert';
 import * as cdk from '@aws-cdk/core';
-import * as CiCd from '../lib/ci-cd-stack';
+import * as Serverless from '../lib/serverless-stack';
 
 test('Empty Stack', () => {
     const app = new cdk.App();
     // WHEN
-    const stack = new CiCd.CiCdStack(app, 'MyTestStack');
+    const stack = new Serverless.ServerlessStack(app, 'MyTestStack');
     // THEN
     expectCDK(stack).to(matchTemplate({
       "Resources": {}
